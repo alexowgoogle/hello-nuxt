7c2b0ce410cef39cf3b24478c0911352e4f1b60b
Xiaoran Wang <alexow@google.com>
2023-07-19 11:50:36 -0700

    init

---
+++ app.vue
@@ -0,0 +1,18 @@
+<style>
+nav .router-link-active {
+  font-weight: bold;
+}
+</style>
+<template>
+  <nav>
+    <ul>
+      <li><NuxtLink to="/">Discussions</NuxtLink></li>
+      <li><NuxtLink to="/about">About</NuxtLink></li>
+    </ul>
+  </nav>
+  <main>
+    <NuxtLayout>
+      <NuxtPage/>
+    </NuxtLayout>
+  </main>
+</template>
